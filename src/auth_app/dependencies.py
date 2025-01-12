a18c3bbd9ba9a3304ea89faa458353e644c63d46
riddler_rid <unkownrid@gmail.com>
2025-01-12 11:20:12 +0300

    add user dependencies.py

--- src/auth_app/dependencies.py
+++ src/auth_app/dependencies.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timezone
+from typing import Annotated
 
 from fastapi import Depends, Request
-from jose import JWTError, jwt
+import jwt
+from jwt import InvalidTokenError
 
 from src.auth_app.models import User
 from src.auth_app.service import AuthService
@@ -24,9 +26,9 @@ async def get_current_user_id(access_token: str = Depends(get_access_token)) -> 
     """Позволяет получить текущего пользователя."""
     try:
         payload = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
-    except JWTError:
+    except InvalidTokenError:
         raise TokenInvalidException
-    user_id: int = int(payload.get("sub"))
+    user_id: int = int(payload.get("subject"))
     expire = payload.get("exp")
     if expire < datetime.now(timezone.utc).timestamp():
         raise TokenExpireException
@@ -39,7 +41,7 @@ async def get_current_user(
     """Позволяет получить текущего пользователя."""
     try:
         payload = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
-    except JWTError:
+    except InvalidTokenError:
         raise TokenInvalidException
     user_id: int = int(payload.get("subject"))
     expire = payload.get("exp")
@@ -48,3 +50,13 @@ async def get_current_user(
 
     user: User | None = await AuthService.get_one_by_id(user_id)
     return user
+
+
+UserDep = Annotated[User, Depends(get_current_user)]
+
+
+async def get_admin_user(admin: UserDep):
+    """Проверяет юзера на администратора"""
+    if not admin.is_admin:
+        raise NotAuthUserException
+    return admin
--- src/post_app/router.py
+++ src/post_app/router.py
@@ -1,15 +1,13 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter
 
-from src.auth_app.dependencies import get_current_user
-from src.auth_app.models import User
+from src.auth_app.dependencies import UserDep
 from src.post_app.schemas import PostSchema
 from src.post_app.service import PostService
 
 router = APIRouter(prefix="/post", tags=["post"])
 
 @router.get("")
-async def get_all_posts(user: User = Depends(get_current_user)):
-    print(f"{user=}")
+async def get_all_posts():
     return await PostService.get_all()
 
 
@@ -20,6 +18,6 @@ async def get_post(id: int):
 
 
 @router.post("")
-async def create_post(post_data: PostSchema):
+async def create_post(post_data: PostSchema, user: UserDep):
     return await PostService.create(post_data)
 
