5c845c4afaead8069bb421d6e64b3cb020e336e1
riddler_rid <unkownrid@gmail.com>
2025-03-18 14:30:47 +0300

    add community_detail.html

--- src/community/router.py
+++ src/community/router.py
@@ -16,6 +16,13 @@ async def get_all_communities(session: SessionDep):
     )
 
 
+@router.get("/{community_id}")
+async def get_community_details(session: SessionDep, community_id: int):
+    return await CommunityService.get_one_by_id(
+        session, community_id, options=[joinedload(Community.posts)]
+    )
+
+
 @router.post("")
 async def create_community(session: SessionDep, user: UserDep, data: CommunityCreate):
     return await CommunityService.create_community(session, user.id, data)
@@ -23,8 +30,8 @@ async def create_community(session: SessionDep, user: UserDep, data: CommunityCr
 
 @router.post("{community_id}/join")
 async def join_existing_community(
-    community_id: int,
-    session: SessionDep,
-    user: UserDep,
+        community_id: int,
+        session: SessionDep,
+        user: UserDep,
 ):
     return await CommunityMemberService.join_community(session, user.id, community_id)
--- src/pages/community_router.py
+++ src/pages/community_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 
 from src.auth.dependencies import UserDep
-from src.community.router import get_all_communities
+from src.community.router import get_all_communities, get_community_details
 from src.templates import templates
 
 router = APIRouter(prefix="/community", tags=["page_community"])
@@ -15,4 +15,15 @@ async def get_communities_page(
     return templates.TemplateResponse(
         name="community/community_list.html",
         context={"request": request, "user": user, "communities": communities},
+    )
+
+@router.get("/{community_id}")
+async def get_community_details_page(
+    request: Request,
+    user: UserDep,
+    community=Depends(get_community_details),
+):
+    return templates.TemplateResponse(
+        name="community/community_detail.html",
+        context={"request": request, "community": community, "user": user},
     )
