763cbac1f9679826b3283b876bb2c7fed1b94f44
riddler_rid <unkownrid@gmail.com>
2025-03-22 12:53:54 +0300

    add dev.track route

--- src/dev_app/router.py
+++ src/dev_app/router.py
@@ -2,6 +2,11 @@ import httpx
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import text
 
+import gpxpy
+import gpxpy.gpx
+import folium
+from datetime import timedelta, datetime
+
 from src.database import engine, Base, SessionDep
 
 router = APIRouter(prefix="/dev", tags=["dev"])
@@ -43,3 +48,72 @@ async def get_github_commits(owner: str, repo: str, limit: int = 5):
             )
         commits = response.json()
         return commits[:limit]  # Возвращаем только последние `limit` коммитов
+
+
+
+@router.get("/track")
+async def get_track_stat():
+
+    # Загрузка GPX-файла
+    with open('src/static/tracks/track.gpx', 'r') as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+    # Инициализация переменных
+    total_distance = 0  # Общее расстояние
+    total_time = timedelta()  # Общее время
+    total_elevation_gain = 0  # Общий набор высоты
+    points = []  # Список всех точек трека
+
+    # Обработка треков и сегментов
+    for track in gpx.tracks:
+        for segment in track.segments:
+            points.extend(segment.points)
+            for i in range(1, len(segment.points)):
+                prev_point = segment.points[i - 1]
+                curr_point = segment.points[i]
+
+                # Расчет расстояния между точками
+                distance = prev_point.distance_2d(curr_point)
+                total_distance += distance
+
+                # Расчет времени между точками
+                time_diff = curr_point.time - prev_point.time
+                total_time += time_diff
+
+                # Расчет набора высоты
+                elevation_diff = curr_point.elevation - prev_point.elevation
+                if elevation_diff > 0:
+                    total_elevation_gain += elevation_diff
+
+    # Расчет средней скорости (в км/ч)
+    if total_time.total_seconds() > 0:
+        avg_speed = (total_distance / 1000) / (total_time.total_seconds() / 3600)
+    else:
+        avg_speed = 0
+
+
+    # Создание карты
+    map_center = [points[0].latitude, points[0].longitude]
+    m = folium.Map(location=map_center, zoom_start=13)
+
+    # Добавление маршрута
+    folium.PolyLine(
+        locations=[[point.latitude, point.longitude] for point in points],
+        color='blue'
+    ).add_to(m)
+
+    # Текущее время
+    now = datetime.now()
+
+    # Количество секунд с 1 января 1970 года
+    file_name = now.timestamp()
+
+    # Сохранение карты в HTML
+    m.save(f'src/static/maps/{file_name}_map.html')
+
+    return {
+        "Общее расстояние": f"{total_distance / 1000:.2f} км",
+        "Общее время": str(total_time),
+        "Общий набор высоты": f"{total_elevation_gain:.2f} м",
+        "Средняя скорость": f"{avg_speed:.2f} км/ч",
+    }
