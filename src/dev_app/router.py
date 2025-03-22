c737403c31d7e423844ca238b077f117bd084c10
riddler_rid <unkownrid@gmail.com>
2025-03-22 13:41:05 +0300

    add dev.uploadgpx

--- src/dev_app/router.py
+++ src/dev_app/router.py
@@ -1,11 +1,15 @@
+import os
+
 import httpx
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from sqlalchemy import text
 
 import gpxpy
 import gpxpy.gpx
 import folium
 from datetime import timedelta, datetime
+
+from starlette.responses import JSONResponse
 
 from src.database import engine, Base, SessionDep
 
@@ -51,12 +55,52 @@ async def get_github_commits(owner: str, repo: str, limit: int = 5):
 
 
 
-@router.get("/track")
-async def get_track_stat():
+@router.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    # Проверка типа файла (опционально)
+    if not file.content_type.startswith('image/'):
+        return JSONResponse(status_code=400, content={"message": "Файл должен быть изображением"})
 
-    # Загрузка GPX-файла
-    with open('src/static/tracks/track.gpx', 'r') as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
+    # Чтение файла
+    contents = await file.read()
+
+    # Сохранение файла на сервере (пример)
+    with open(f"src/static/images/uploads/{file.filename}", "wb") as f:
+        f.write(contents)
+
+    return {"filename": file.filename, "content_type": file.content_type}
+
+
+@router.post("/uploadgpx/")
+async def upload_gpx_file(file: UploadFile = File(...)):
+    # Проверка типа файла
+    if file.content_type != "application/gpx+xml":
+        raise HTTPException(status_code=400, detail="Файл должен быть GPX")
+
+    # Текущее время
+    now = datetime.now()
+
+    # Количество секунд с 1 января 1970 года
+    file_name = now.timestamp()
+
+
+    gpx_file_name = f"{now.timestamp()}.gpx"
+
+    # Чтение файла
+    contents = await file.read()
+
+    # Сохранение файла на сервере
+    upload_dir = "src/static/tracks"
+    os.makedirs(upload_dir, exist_ok=True)  # Создание директории, если её нет
+    file_path = os.path.join(upload_dir, gpx_file_name)
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    # Парсинг GPX-файла
+    try:
+        gpx = gpxpy.parse(contents)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Ошибка при парсинге GPX-файла: {str(e)}")
 
     # Инициализация переменных
     total_distance = 0  # Общее расстояние
@@ -77,20 +121,21 @@ async def get_track_stat():
                 total_distance += distance
 
                 # Расчет времени между точками
-                time_diff = curr_point.time - prev_point.time
-                total_time += time_diff
+                if prev_point.time and curr_point.time:
+                    time_diff = curr_point.time - prev_point.time
+                    total_time += time_diff
 
                 # Расчет набора высоты
-                elevation_diff = curr_point.elevation - prev_point.elevation
-                if elevation_diff > 0:
-                    total_elevation_gain += elevation_diff
+                if prev_point.elevation and curr_point.elevation:
+                    elevation_diff = curr_point.elevation - prev_point.elevation
+                    if elevation_diff > 0:
+                        total_elevation_gain += elevation_diff
 
     # Расчет средней скорости (в км/ч)
     if total_time.total_seconds() > 0:
         avg_speed = (total_distance / 1000) / (total_time.total_seconds() / 3600)
     else:
         avg_speed = 0
-
 
     # Создание карты
     map_center = [points[0].latitude, points[0].longitude]
@@ -102,18 +147,14 @@ async def get_track_stat():
         color='blue'
     ).add_to(m)
 
-    # Текущее время
-    now = datetime.now()
-
-    # Количество секунд с 1 января 1970 года
-    file_name = now.timestamp()
-
     # Сохранение карты в HTML
     m.save(f'src/static/maps/{file_name}_map.html')
 
-    return {
-        "Общее расстояние": f"{total_distance / 1000:.2f} км",
-        "Общее время": str(total_time),
-        "Общий набор высоты": f"{total_elevation_gain:.2f} м",
-        "Средняя скорость": f"{avg_speed:.2f} км/ч",
-    }
+    # Возврат результата
+    return JSONResponse(content={
+        "filename": file.filename,
+        "total_distance_km": f"{total_distance / 1000:.2f}",
+        "total_time": str(total_time),
+        "total_elevation_gain_m": f"{total_elevation_gain:.2f}",
+        "avg_speed_kmh": f"{avg_speed:.2f}",
+    })
